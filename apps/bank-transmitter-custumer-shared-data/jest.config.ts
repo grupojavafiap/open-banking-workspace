ace3a7e72445306fe6d97f1ab0d581a74b5ce22a
Benisson Lopes <benisson@gmail.com>
2022-11-19 10:21:36 -0300

    feat: estrutura inicial do projeto

---
+++ apps/bank-transmitter-custumer-shared-data/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'bank-transmitter-custumer-shared-data',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory:
+    '../../coverage/apps/bank-transmitter-custumer-shared-data',
+};
